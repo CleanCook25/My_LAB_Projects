b0a955fc91c3bc5bbcd891210f76f4486f2233c5
Aqib Idreeshi K21 <2k23.mca2312076@gmail.com>
2025-08-05 22:20:24 +0530

    i am seeing 2 files ops

---
+++ program3.py
@@ -0,0 +1,11 @@
+#MY PROGRAM 3 IS HERE 
+        
+def find_missing(lst):
+    return sorted(set(range(lst[0], lst[-1])) - set(lst))
+
+# Driver code
+lst = [1, 2, 4, 6, 7, 9, 10]
+print(find_missing(lst))
+#MY PROGRAM 3 IS HERE 
+#MY PROGRAM 3 IS HERE 
+#MY PROGRAM 3 IS HERE 
